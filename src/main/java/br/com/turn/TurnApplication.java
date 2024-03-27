78ea73f5155775355e0629ae62d8de617378c92b
antoniiux <antoniiotales@gmail.com>
2024-03-27 15:17:08 -0300

    Criado projeto simples no Spring Boot no start.spring.io

---
+++ src/main/java/br/com/turn/TurnApplication.java
@@ -0,0 +1,13 @@
+package br.com.turn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/turn/TurnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.turn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
